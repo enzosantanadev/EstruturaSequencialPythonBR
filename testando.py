097341ca61cbcac82961e77450a56596cccc8b49
Enzo Santana <161235284+enzosantanadev@users.noreply.github.com>
2024-09-23 12:18:02 -0300

    Add files via upload

---
+++ testando.py
@@ -0,0 +1,10 @@
+celsius = float(input("informe a temperatura em celsius"))
+
+def transformar_temperatura(celsius):
+    return (celsius * 1.8) + 32
+resultado = transformar_temperatura(celsius)
+
+print(resultado)
+
+
+
